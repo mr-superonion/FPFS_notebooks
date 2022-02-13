5eed2a6204feb5e6f0db3588f58b7c3c7e27f69f
Xiangchong Li <xiangchong.li@ipmu.jp>
2022-02-14 04:50:52 +0900

    mover to server

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Change the Dir variable to the current directory
+
+Dir="$homeSys/code/FPFS/"
+export PATH="$Dir/examples/":$PATH
+export PYTHONPATH="$Dir/examples/":$PYTHONPATH
+
+Dir2="$homeWrk/FPFS2/sdet_notebooks/"
+export PATH="$Dir2/run/":$PATH
+export PYTHONPATH="$Dir2/run/":$PYTHONPATH
+
+
+DirBase="$homeSys/code/FPFS_Private/FPFSBASE/"
+export PYTHONPATH="$DirBase/python/":$PYTHONPATH
+
